a717d750d22ded8267f904c5e57c338104de82df
miamarichka <ilchyshyn.marichka@gmail.com>
2023-07-27 13:55:19 +0300

    removing statement

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -5,7 +5,7 @@ import { Navigate } from "react-router-dom";
 
 export const PrivateRoute = ({ component: Component, redirectTo = "/" }) => {
   const { isLoggedIn, isRefreshing } = useAuth();
-  console.log(redirectTo)
+
   const shouldRedirect = !isLoggedIn && !isRefreshing;
 
   return shouldRedirect ? <Navigate to={redirectTo} /> : <Component/>;
